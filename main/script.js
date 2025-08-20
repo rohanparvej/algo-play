eab803f7e22604c3971b75372ecc8381e42c0cfe
rohanparvej <152258212+rohanparvej@users.noreply.github.com>
2025-08-20 12:20:13 +0530

    Delete main directory

--- main/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", () => {
-  const bubbleBtn = document.getElementById("bubble-btn");
-
-  bubbleBtn.addEventListener("click", () => {
-    window.location.href = "bubble.html"; // navigate to bubble sort page
-  });
-});
